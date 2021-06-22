37d782fb71468a14f305a900f57f8ffa65cb2835
Seraphina Oney <soney@RCSs-MacBook-Pro-9.local>
2021-06-22 11:50:58 -0700

    fix routes for this week and next

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import Welcome from './Routes/Home'
 import Week1 from './Routes/Week1'
 import Week2 from './Routes/Week2'
 import Week3 from './Routes/Week3'
-import Week4 from './Routes/Week3'
+import Week4 from './Routes/Week4'
+import Week5 from './Routes/Week5'
 import Syllabus from './Routes/Syllabus'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 
@@ -15,7 +16,8 @@ const App = () => {
         <Route path="/week1/:index?" component={Week1} />
         <Route path="/week2/:index?" component={Week2} />
         <Route path="/week3/:index?" component={Week3} />
-        <Route path="/week4/:index?" component={Week3} />
+        <Route path="/week4/:index?" component={Week4} />
+        <Route path="/week5/:index?" component={Week5} />
         <Route path="/syllabus" exact component={Syllabus} />
       </Router>
     </div>
