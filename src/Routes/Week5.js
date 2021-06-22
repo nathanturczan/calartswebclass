33a9b4f8813ac23bd2a81cdb577661b679b832de
Seraphina Oney <soney@RCSs-MacBook-Pro-9.local>
2021-06-22 11:52:15 -0700

    add template for week 5

---
+++ src/Routes/Week5.js
@@ -0,0 +1,67 @@
+import React from 'react'
+import { Slideshow, Slide, SlidePink } from '../components/Slideshow'
+import styled from 'styled-components'
+import {
+  Title,
+  Subtitle,
+  Text,
+  List,
+  Item,
+  VertCenter,
+  TextCenter,
+  ReadabilityBlock,
+  CenterReadabilityBlock,
+  Half,
+  TwoHalvesLayout,
+  key,
+  colon,
+  value,
+  semicolon
+} from './UI'
+import * as img from '../assets/week-4'
+
+
+const Week4 = () => (
+  <Slideshow>
+    <Slide>
+      <VertCenter>
+        <Title>Week 4</Title>
+        <Subtitle>Media and Web Development</Subtitle>
+        <Text>
+          <em>Advanced CSS</em>
+        </Text>
+      </VertCenter>
+    </Slide>
+    <Slide>
+      <VertCenter>
+        <Title>Lecture Outline</Title>
+        <List>
+          <Item>
+            <Text>review / housekeeping</Text>
+          </Item>
+          <Item>
+            <Text>positioning and layout continued</Text>
+          </Item>
+          <Item>
+            <Text>background images / effects</Text>
+          </Item>
+          <Item>
+            <Text>embedded content</Text>
+          </Item>
+          <Item>
+            <Text>pseudo-selectors</Text>
+          </Item>
+        </List>
+      </VertCenter>
+    </Slide>
+    <Slide>
+      <VertCenter>
+        <Subtitle>Closing Tags and Quoted Attributes</Subtitle>
+        <img width="60%" src={img.tags} alt="" />
+      </VertCenter>
+    </Slide>
+    
+  </Slideshow>
+)
+
+export default Week4
